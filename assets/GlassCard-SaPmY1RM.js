22a7f6729bde7c7a63f57299c70999401768043c
Developer <developer@example.com>
2026-09-10 21:07:10 +0530

    Updates

---
+++ assets/GlassCard-SaPmY1RM.js
@@ -0,0 +1,5 @@
+import{j as t}from"./index-DQifa7M1.js";function d({children:a,className:s="",glow:r=!1,onClick:l}){return t.jsx("div",{onClick:l,className:`
+        relative rounded-3xl p-6 md:p-8 transition-all duration-300
+        ${r?"glass-card-glow":"glass-card"}
+        ${s}
+      `,children:a})}export{d as G};
